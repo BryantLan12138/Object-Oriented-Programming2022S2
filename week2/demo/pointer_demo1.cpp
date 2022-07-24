dd1d3ece7f66e3376ee7d76045ebfe96e40fe723
BryantLan12138 <bryant.lan1212@gmail.com>
2022-07-24 10:45:16 +0930

    re-commit week2 files

---
+++ week2/demo/pointer_demo1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int * ptr;
+    int i;
+
+    // above initialization equivelant to int *ptr, i;
+    i = 11;
+    ptr = &i;
+
+    cout << "Value of ptr is: " << ptr << endl;
+    // dereferencing 
+    cout << "Value of i is: " << *ptr << endl;
+}
