7af941356d8c4fcdfe76f21b798d69b3be53a81b
BryantLan12138 <bryant.lan1212@gmail.com>
2022-08-14 13:52:19 +0930

    change

---
+++ WS3/demo.cpp
@@ -0,0 +1,14 @@
+// for(int i = 0; i < 10; i++) 
+// {
+//     for(int j = 0; j < 10; j++) 
+//     {
+//         if(j != 9) 
+//         {
+//             cout << array[i][j] << " ";
+//         }
+//         else
+//         {
+//             cout << array[i][j] << endl;
+//         }
+//     }
+// }
--- WS3/main-2-5.cpp
+++ WS3/main-2-5.cpp
@@ -5,15 +5,39 @@ using namespace std;
 int main(){
 
 	double array[2][2] = {{0.5,1.5},{2.5,3.5}};
-
+	// double array_1d[2] = {0};
+	// double* twoptr = array_1d;
 	double* ptr = array[0];
 
 	for(int i=0; i<2; i++){
-		for(int j=0; j<1; j++){
-			cout<<*(ptr+2*i+j)<<" ";
+		// for(int j=0; j<1; j++){
+		for(int j=0; j<2; j++){
+			if(j != 1)
+				cout<<*(ptr+2*i+j)<<" ";
+				//array[0][1]
+				//array[1][0]
+			else
+				cout<<*(ptr+2*i+1)<<endl;
+
 		}
-		cout<<*(ptr+2*i+1)<<endl;
 	}
 
 	return 0;
 }
+/** 0.5 ,1.5
+ ** 2.5, 3.5 
+ ** 4.5, 5.5
+ */
+//   ^
+//   |
+//  ptr
+
+// 	*(ptr) = 0.5
+// 	*(ptr + j) = 1.5
+
+//  0x123120
+//  0x123128
+
+//  bits byte
+//  int/float takes 4 byte
+//  double takes 8 byte
